be53bc1eeb8062dbdc704945522b436eb15cdff7
cytgrzechu <80256145+cytgrzechu@users.noreply.github.com>
2022-05-31 16:21:53 +0200

    poprawione zadanie z testami

---
+++ 4/main.py
@@ -0,0 +1,25 @@
+import math
+
+class Calculator:
+    def add(self, x, y):
+        return x + y
+
+
+    def subtract(self, x, y):
+        return x - y
+
+
+    def divide(self, x, y):   
+        return x / y
+
+
+    def multiply(self, x, y):
+        return x * y
+
+
+    def square(self,x):
+        return math.sqrt(x)
+
+
+    def power(self, x, y):
+        return math.pow(x,y) 
